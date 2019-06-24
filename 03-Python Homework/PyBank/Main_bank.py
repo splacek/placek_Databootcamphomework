e782ec61d80bd54c4584f6d0486e7314d3ec1777
splacek <splacek8065@gmail.com>
2019-06-24 15:01:50 -0400

    Python 3 homework

---
+++ 03-Python Homework/PyBank/Main_bank.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 24 14:45:57 2019
+
+@author: splac
+"""
+pybank
---
+++ 03-Python Homework/PyPoli/main_poli.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 24 14:48:26 2019
+
+@author: splac
+"""
+
+pypoli
